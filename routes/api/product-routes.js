dbdd54a80e41d06586292a363e6f0430227bc42c
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-22 23:29:16 -0400

    get one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,20 @@ router.get('/', async (req, res) => {
   });
 
   // GET ONE PRODUCT
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      include: [
+        { model: Category },
+        { model: Tag },
+      ]
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
