9bc25f1354d1f40700388e56dd504dc8fef3a16d
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-23 19:57:50 -0400

    update and delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,15 +32,30 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  // create a new category
+  Tag.create(req.body)
+  res.status(200).json('Created new tag ' + req.body.tag_name)
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // update a category by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  res.status(200).json('updated '+ req.body.tag_name);
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete one product by its `id` value
+  try {
+    const tagData = await Tag.findByPk(req.params.id)
+    await tagData.destroy();
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
