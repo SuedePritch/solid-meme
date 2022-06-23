2316b4b842c2f78927c4b039c156e3ff05117e45
James Pritchard <james.edward.pritchard@gmail.com>
2022-06-23 13:40:27 -0400

    new category

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey:'category_id',
+  foreignKey:'id',
   onDelete:'CASCADE'
 })
 // Categories have many Products
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,7 +36,8 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create()
+  Category.create(req.body)
+  res.status(200).json('Created new Category ' + req.body.category_name)
 });
 
 router.put('/:id', (req, res) => {
